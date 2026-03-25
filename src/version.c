0de80179302aace54bf15d4e8e3a1e0c50e934e6
Codacy Archive <codacy-archive@local>
2026-03-25 01:20:23 +0000

    snapshot 2026-03-25T01:20:23.259157+00:00

--- src/version.c
+++ src/version.c
@@ -735,6 +735,16 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    240,
+/**/
+    239,
+/**/
+    238,
+/**/
+    237,
+/**/
+    236,
+/**/
     235,
 /**/
     234,
